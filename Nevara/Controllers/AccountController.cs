88488dbc46296816ca6302fb1e40d942226d7efa
tyntin091 <tyntin091@gmail.com>
2018-06-30 17:09:49 +0700

    refresh project

---
+++ Nevara/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Nevara.Areas.Admin.Models;
+using Nevara.Models.Entities;
+
+namespace Nevara.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+          
+        }
+        [HttpGet]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [Route("register.html")]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            //MM/dd/yyy
+            var user = new AppUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FullName = model.FullName,
+                PhoneNumber = model.PhoneNumber,      
+                Avatar = string.Empty
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+           
+            if (result.Succeeded)
+            {
+                await _userManager.SetPhoneNumberAsync(user, user.PhoneNumber);
+                await _userManager.AddToRoleAsync(user, "Customer");
+                return RedirectToLocal(returnUrl);
+            }
+            AddErrors(result);
+            // a
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return Redirect("/account/login");
+            }
+        }
+
+        #endregion
+    }
+
+}
